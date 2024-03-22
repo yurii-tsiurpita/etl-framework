3ff613714934e241ba96a0a897580fea317c4224
yurii-tsiurpita <yurii.tsiurpita@nikcode.net>
2024-03-22 22:05:21 +0200

    refactor: Refactor ai assistants

--- ai_assistants/figma_ai_assistant/figma_ai_assistant.py
+++ ai_assistants/figma_ai_assistant/figma_ai_assistant.py
@@ -1,20 +1,22 @@
 from langchain_community.vectorstores.chroma import Chroma
 from langchain.prompts import PromptTemplate
 from ai_assistants.ai_assistant import AiAssistant
+from langchain_core.messages import AIMessage, HumanMessage
 
 class FigmaAiAssistant(AiAssistant):
+  data_source_name = 'Figma'
+
   def __init__(self, chroma: Chroma):
     super().__init__(chroma)
 
-  def _generatePrompt(self, query: str, chat_history: list) -> str:
-    prompt_template = PromptTemplate.from_template(
-      """
-        You are a Figma chatbot assistant. Your name is Trinity. Feel free to share this information.
-        Answer the question in English language as truthfully and helpfully as possible based on all the information you have and provided chat history below.
-        If you don't know the answer, say that you don't know, don't try to make up an answer.
-        Chat history: {chat_history}
-        Question: {question}
-      """
+  def _generatePrompt(
+    self,
+    prompt_template: PromptTemplate,
+    query: str,
+    chat_history: list[AIMessage | HumanMessage],
+  ) -> str:
+    return prompt_template.format(
+      question=query,
+      chat_history=chat_history,
+      data_source_name=self.data_source_name,
     )
-
-    return prompt_template.format(question=query, chat_history=chat_history)
