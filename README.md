a034b95ece1ae53fa00352483e57ab122d7c9d0d
yurii-tsiurpita <yurii.tsiurpita@nikcode.net>
2024-03-24 19:11:12 +0100

    fix: Remove sharepoint integration

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-AI Assistant
+Etl-framework + AI Assistant
--- etls/constants/etl_constants.py
+++ etls/constants/etl_constants.py
@@ -1,3 +1,2 @@
 CONFLUENCE_CHROMA_NAME = 'confluence_chroma'
-SHAREPOINT_CHROMA_NAME = 'sharepoint_chroma'
 FIGMA_CHROMA_NAME = 'figma_chroma'
